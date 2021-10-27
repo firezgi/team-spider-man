aa3c157e612d89f18c32cca7580710e9fde16ca8
Habteab Firezgi <keshi2017@gmail.com>
2021-10-26 21:15:47 -0700

    added footer component

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { posts, users} from './components/WPAPI';
+import Footer from './components/Footer';
 
 export default function App() {
   posts();
+  // console.log(posts)
   users();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* {posts.categories} */}
+      <Footer/>
       <StatusBar style="auto" />
     </View>
   );
