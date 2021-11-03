d3b63ed92d75502dcf0e3e33264e43977231890c
Jason Smith <jasons@alphaworks.tech>
2021-11-03 14:57:00 -0700

    refectoroed code for accessing api and how it's being used

--- components/ProfilePage.js
+++ components/ProfilePage.js
@@ -1,52 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import ThemeLoggedIn from "./ThemeLoggedIn";
-import { media, users, members, WP_GET } from "./WPAPI";
+import { WP_GET } from "./WPAPI";
 
-export default function ProfilePage({ navigation }) {
+export default function ProfilePage({ navigation, userId = 1 }) {
 
-    // console.log(media()); <--- For test
-
-    // media function from WPAPI wasn't working right
-    // Currently is set up for testing
-    // const images = () => {
-    //     return fetch('https://jualuc1.dreamhosters.com/buddypress/v1')
-    //         .then(response => response.json())
-    //         .catch(error => error)
-    // }
-    // console.log(images()); <-- For testing
-
-    let userId = 1;
-
-
-    const [profileImage, setProfileImage] = useState([]);
-    useEffect(() => media()
+    const [buddypressData, setBuddypressData] = useState([]);
+    useEffect(() => WP_GET("members", `/${userId}`)
                     .then(
-                        (data) => setProfileImage(data)
+                        (data) => {
+                            setBuddypressData(data);
+                            console.log('Buddypress data: ',data);
+                        }
                     ), []);
-    // console.log(profileImage); <-- For testing
-
-
-    const getProfileImage = profileImage.filter(img => (img.author === userId));
-    const imgData = getProfileImage.map(data => {
-        return data.source_url;
-    });
-    const profileImg = imgData[0];
-    console.log(getProfileImage);
 
     const [userData, setUserData] = useState([]);
-    useEffect(() => users().then((data) => setUserData(data)), []);
-    console.log(userData);
-
-    const getUserData = userData.filter(user => (user.id === userId));
-    const userDisplayName = getUserData.map(name => {
-        return name.name;
-    })
-
-    const userDescription = getUserData.map(about => {
-        return about.description;
-    })
-
+    useEffect(() => WP_GET("users", `/${userId}`)
+                    .then((data) => {
+                        setUserData(data);
+                        console.log('Wordpress user data: ',data);
+                    }), []);
+    
 
     return (
         <ThemeLoggedIn navigation={navigation}>
@@ -54,16 +28,20 @@ export default function ProfilePage({ navigation }) {
                 <View style={profileStyles.profileLeft}>
                     <Image
                         style={profileStyles.profileImage}
-                        source={profileImg}
+                        source={buddypressData.avatar_urls?.full}
                     />
                     <View style={profileStyles.nameWrap}>
-                        <Text>{userDisplayName}</Text>
+                        <Text>{userData.name}</Text>
                     </View>
+                    {/* MAKE Linkable */}
+                    <Button
+                        title="Edit Profile"
+                    />
                 </View>
                 <View style={profileStyles.profileRight}>
                     <Text>About Me</Text>
                     <View style={profileStyles.descriptionWrap}>
-                        <Text>{userDescription}</Text>
+                        <Text>{userData.description}</Text>
                     </View>
                 </View>
             </View>
@@ -74,16 +52,13 @@ export default function ProfilePage({ navigation }) {
 const profileStyles = StyleSheet.create({
     profileWrap: {
         display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
         justifyContent: 'center',
         alignItems: 'flex-start',
-        maxWidth: '960px',
+        width: '960px',
         margin: 'auto',
     },
     profileLeft: {
-        maxWidth: '250px',
-        height: 400,
+        width: '100%',
         borderWidth: '1px',
         borderColor: '#000',
         borderStyle: 'solid',
@@ -92,7 +67,8 @@ const profileStyles = StyleSheet.create({
         alignItems: 'center',
     },
     profileRight: {
-        flex: 1,
+        // flex: 1,
+        width: '100%',
         height: 400,
         borderWidth: '1px',
         borderColor: '#000',
@@ -101,8 +77,8 @@ const profileStyles = StyleSheet.create({
         margin: '5px',
     },
     profileImage: {
-        width: '200px',
-        height: '200px',
+        width: '150px',
+        height: '150px',
         borderRadius: '10px',
         margin: '5px',
     }
--- components/WPAPI.js
+++ components/WPAPI.js
@@ -8,7 +8,11 @@ const endpoint = {
     search: '/wp/v2/search',
     blockTypes: '/wp/v2/blockTypes',
     blocks: '/wp/v2/blocks',
-    pages: '/wp/v2/pages'
+    pages: '/wp/v2/pages',
+    members: '/buddypress/v1/members',
+    groups: '/buddypress/v1/xprofile/groups',
+    activity: '/buddypress/v1/activity',
+    notifications: '/buddypress/v1/notifications',
 };
 
 export const WP_GET = (type, queryStringVars = '') => {
@@ -22,57 +26,3 @@ export const WP_GET = (type, queryStringVars = '') => {
         .then(response => response.json())
         .catch(error=> console.log(error))
 }
-
-
-
-// export const members = () => {
-//     return fetch(`${BUDDY_URL}/wp/v2/posts`).then(response => response.json())
-//     .catch(error=>error)
-// }
-
-// import React from 'react'
-
-
-// const BASE_URL = 'https://jualuc1.dreamhosters.com/wp-json';
-
-
-// export const posts= ()=> {
-//     return fetch(`${BASE_URL}/wp/v2/posts`).then(response => response.json())
-//     .catch(error=>error)
-// }
-
-export const users =()=>{
-    return fetch(`${BASE_URL}/wp/v2/users`).then(response => response.json())
-    .catch(error=>error)
-}
-
-export const media =()=>{
-    return fetch(`${BASE_URL}/wp/v2/media`).then(response => response.json())
-    .catch(error=>error)
-}
-
-// export const comments =()=>{
-//     return fetch(`${BASE_URL}/wp/v2/comments`).then(response => response.json())
-//     .catch(error=>error)
-// }
-
-// export const categories =()=>{
-//     return fetch(`${BASE_URL}/wp/v2/categories`).then(response => response.json())
-//     .catch(error=>error)
-// }
-
-// export const search =()=>{
-//     return fetch(`${BASE_URL}/wp/v2/search`).then(response => response.json())
-//     .catch(error=>error)
-// }
-
-// export const blockTypes =()=>{
-//     return fetch(`${BASE_URL}/wp/v2/block-types`).then(response => response.json())
-//     .catch(error=>error)
-// }
-
-// export const blocks=()=>{
-//     return fetch(`${BASE_URL}/wp/v2/blocks`).then(response => response.json())
-//     .catch(error=>error)
-// }
-
